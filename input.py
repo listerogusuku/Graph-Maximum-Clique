fa384f3389f44794afbaa02a7ede7abcfdc92709
listerogusuku <listeror@al.insper.edu.br>
2023-12-09 13:02:20 -0300

    Sobe analise grafica

--- input.py
+++ input.py
@@ -2,14 +2,14 @@ import networkx as nx
 import random
 
 #Parâmetros:
-num_vertices = 5  #Vértices no grafo
+qnt_vertices = 5  #Vértices no grafo
 probabilidade_conexao = 0.7  #Chances de haver uma aresta entre dois vértices (ajuste conforme necessário)
 
-grafo = nx.fast_gnp_random_graph(num_vertices, probabilidade_conexao) #Grafo aleatório densamente conectado
+grafo = nx.fast_gnp_random_graph(qnt_vertices, probabilidade_conexao) #Grafo aleatório densamente conectado
 
 nome_arquivo = "grafo.txt"
 with open(nome_arquivo, 'w') as arquivo:
-    arquivo.write(f"{num_vertices} {grafo.number_of_edges()}\n")
+    arquivo.write(f"{qnt_vertices} {grafo.number_of_edges()}\n")
 
     for aresta in grafo.edges():
         arquivo.write(f"{aresta[0]+1} {aresta[1]+1}\n")  
